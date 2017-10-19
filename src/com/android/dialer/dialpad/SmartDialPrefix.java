3a821dc0216428c9b4aa3d55c94b6cf99d963d70
martincz <martincz@mokeedev.com>
2017-10-19 20:05:10 +0800

    Revert "Hebrew T9 for Dialer"
    This reverts commit 5dcd954f7cb24f5812fef60d02dc10ad4d73b5fd.

--- src/com/android/dialer/dialpad/SmartDialPrefix.java
+++ src/com/android/dialer/dialpad/SmartDialPrefix.java
@@ -96,8 +96,6 @@ public class SmartDialPrefix {
             mMap = new RussianSmartDialMap();
         } else if (locale.equals("GR")) {
             mMap = new GreekSmartDialMap();
-        } else if (locale.equals("IL")) {
-            mMap = new HebrewSmartDialMap();
         } else {
             mMap = new LatinSmartDialMap();
         }
